67c3d1658301a5b7a1e162dfe479ef1306ba6807
Blertin Hamza <blertin.hamza7@gmail.com>
2022-09-16 21:53:35 +0200

    adding transfer repo

---
+++ TiniCenter.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TiniCenter.Models;
+
+namespace TiniCenter.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Language> Languages { get; set; }
+    }
+
+}
---
+++ TiniCenter.Models/Language.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiniCenter.Models
+{
+   public  class Language
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Gjuha")]
+        [StringLength(50)]
+        public string LanguageType { get; set; }
+        public string Name { get; set; }
+
+    }
+}
